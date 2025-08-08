c902439f7454555b9d30f1fd40595f9c736ac98f
abhiram <abhiramajithr@gmail.com>
2025-08-08 21:12:32 +0530

    basic integration

--- app.py
+++ app.py
@@ -9,10 +9,13 @@
 from flask import Flask
 from flask import request, Response
 import uuid
+from flask_cors import CORS
 
 from utils import createTask, viewAllTask
 
 app = Flask(__name__)
+
+CORS(app, resources={r'/*': { "origins": "*"}}, supports_credentials=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -22,10 +25,11 @@ def index():
 @app.route('/create-task', methods=['POST'])
 def create_task():
     if request.method == 'POST':
-        task_name = request.form['task_name']
-        tasks = createTask({ 'name': task_name, 'id': str(uuid.uuid4())})
+        task = request.get_json()
+        tasks = createTask({ 'name': task.get('task_name'), 'id': str(uuid.uuid4())})
         return { 'data': tasks }
     
+
 @app.route('/view-all-data', methods=['GET'])
 def getAllTasks():
     return viewAllTask()
--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,22 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault();
 
     const inputElement = document.querySelector('input');
     const taskName = inputElement.value;
 
     inputElement.value = '';
-    // backend integration
-    console.log(`submited task ${taskName}`);
+    const response = await fetch('http://localhost:4000/create-task', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            'task_name': taskName
+        })
+    })
+
+    const data = await response.json();
+    console.log(data);
 }
 
 document.querySelector('form').addEventListener('submit', handleSubmit);
